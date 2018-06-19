01822973f45c312c1889c423fb4d243c206828e2
Weijing Wang <weijingrocks@gmail.com>
2018-06-19 16:21:09 -0700

    not work

---
+++ calculator.py
@@ -0,0 +1,9 @@
+my_number1 = raw_input("1st number enter")
+my_number2 = raw_input("2nd number enter")
+operator = raw_input("operator enter")
+
+print my_number1
+print operator
+print my_number2
+
+print int(my_number1 + operator + my_number2) 
